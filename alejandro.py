ce3d9ae03cf305e9fd2fb1a4ca6c55e294d841c3
Alejandro Vecchio <avecc004@fiu.edu>
2022-04-15 10:31:40 -0400

    switched from histograms to line charts

--- alejandro.py
+++ alejandro.py
@@ -2,14 +2,14 @@ import requests
 import streamlit as st
 import pandas as pd
 import keys
-import plotly.figure_factory as ff
+import pandas as pd
 
 def fixForURL(string):
-    string = string.replace(" ", "+")
+    string = string.replace(" ", "")
     return string
 
 #copied jada's code into a function for easier retrieval for the histogram
-def ratings(game_to_search_for):
+def ratings_data(game_to_search_for):
     # Used to replace spaces in a search with +'s, so that the URL actually works
 
     games_url = "https://api.rawg.io/api/games?key=" + keys.RAWG_API_KEY + "&search=" + fixForURL(
@@ -32,34 +32,29 @@ def ratings(game_to_search_for):
             game_to_search_for)
         print("The URL of the API request:" + games_url)
 
-        # Creates a dictionary (like an array but the indexes are "keys" (strings) rather than integers) using info returned from the URL request
         games_dict = requests.get(games_url).json()
 
-        ratings_dict = {0: [], 1: [], 2: [], 3: [], 4: [], 5: []}
-
+        ratings_list = [0, 0, 0, 0, 0, 0]
+        reviewers = 0
         for i in games_dict["results"][0]["ratings"]:
             rating_score = i["id"]
             rating_count = i["count"]
-            ratings_dict[rating_score].append(rating_count)
+            reviewers += rating_count
+            ratings_list[rating_score] = rating_count
 
-        ratings_list = []
-        for j in range(6):
-            if (ratings_dict[j].__len__() == 0):
-                ratings_dict[j].append(0)
-            ratings_list.append(ratings_dict[j])
+        for i in range(len(ratings_list)):
+            ratings_list[i] = ratings_list[i]/reviewers
 
         return ratings_list
 
 def compare_game(game1, game2):
-    game1_ratings = ratings(game1)
-    game2_ratings = ratings(game2)
 
-    hist_data = [game1_ratings, game2_ratings]
-    group_labels = [game1, game2]
+    game1_ratings = ratings_data(game1)
+    game2_ratings = ratings_data(game2)
 
-    fig = ff.create_distplot(
-        hist_data, group_labels, bin_size=[0, 1, 2, 3, 4, 5]
-    )
-    st.plotly_chart(fig, use_container_width=True)
+    df = pd.DataFrame(list(zip(game1_ratings, game2_ratings)),
+                      columns = [str(game1).capitalize(), str(game2).capitalize()])
+
+    st.line_chart(df)
 
 compare_game("Portal 2", "Reciever 2")
